6192485fa83b4c544c0329a7f2263ba4a85fe31d
Prajwal Gaonkar <prajwal.gaonkar.x7@gmail.com>
2020-06-11 19:07:14 +0530

    modified colorscheme

--- config.def.h
+++ config.def.h
@@ -3,22 +3,23 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222"; /* background colour */
-static const char col_gray2[]       = "#444444"; /* inactive window border colour */
-static const char col_gray3[]       = "#bbbbbb"; /* font colour */
-static const char col_gray4[]       = "#eeeeee"; /* current tag and current window title font colour */
-static const char col_cyan[]        = "#005577"; /* top bar second colour(default:blue) */
-static const char col_red[]         = "#ff0000"; /* active window border colour */
+static const char normbgcol[]       = "#222222";
+static const char normbrdrcol[]     = "#111111";
+static const char normfgcol[]       = "#bbbbbb";
+static const char selfgcol[]        = "#111111";
+static const char selbgcol[]        = "#bd8a33"; /* bd8a33 */
+static const char selbrdrcol[]      = "#770000";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_red },
+	[SchemeNorm] = { normfgcol, normbgcol, normbrdrcol },
+	[SchemeSel]  = { selfgcol,  selbgcol,  selbrdrcol },
 };
 
 /* tagging */
@@ -59,9 +60,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcol, "-nf", normfgcol, "-sb", selbgcol, "-sf", selfgcol, NULL };
 static const char *termcmd[]  = { "st", NULL };
 /* commands defined by Prajwal */
+static const char *poweroffcmd[] = {"sudo", "poweroff", NULL};
 static const char *rangercmd[] = {"st", "-e" , "ranger", NULL};
 static const char *nmtuicmd[] = {"st", "-e", "nmtui", NULL};
 static const char *refreshbarcmd[] = {"refreshbar", NULL};
@@ -91,6 +93,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -104,7 +109,7 @@ static Key keys[] = {
         { MODKEY,                       XK_r,      spawn,          {.v = rangercmd } },
         { MODKEY,                       XK_n,      spawn,          {.v = nmtuicmd } },
         { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = refreshbarcmd } },
-
+        { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = poweroffcmd } },
 };
 
 /* button definitions */
